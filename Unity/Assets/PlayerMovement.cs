d17577cf2f5f47ca1a20dcc2de12850a010fd214
Rich Metson <richardmetson@gmail.com>
2015-02-12 18:31:50 +0000

    added pixel sprite sheet and divided, (some irregularities, but should be fine)
    added player sprite and basic movement script with fake gravity

---
+++ Unity/Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Vector3 velocity = Vector3.zero;
+	public Vector3 gravity;
+	public Vector3 moveLeftVel;
+	public float maxSpeed;
+
+	bool moveLeft = false;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Do Input and Graphics updates here
+	void Update(){
+		if (Input.GetKeyDown(KeyCode.LeftArrow)){
+			moveLeft = true;
+		}
+	}
+
+	
+	// Do physics engine updates here
+	void FixedUpdate () {
+
+		velocity += gravity * Time.deltaTime;
+
+		if(moveLeft == true){
+			moveLeft = false;
+			velocity += moveLeftVel;
+		}
+
+		velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
+		transform.position += velocity * Time.deltaTime;
+
+
+
+	
+	}
+}
