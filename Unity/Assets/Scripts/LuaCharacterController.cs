385faf56eaf8acd350e12555658da9eb1b017931
Pontus Schönberg <pschonberg@gmail.com>
2015-02-14 20:59:11 +0000

    Movement and animation working, travels correctly with moving platforms.

--- Unity/Assets/Scripts/LuaCharacterController.cs
+++ Unity/Assets/Scripts/LuaCharacterController.cs
@@ -4,12 +4,27 @@ using LuaInterface;
 
 public class LuaCharacterController : MonoBehaviour {
 
+	private float maxHorizontalVelocity = 1.0f;
+	private Vector2 movement;
+
+	public bool grounded = false;
+	public LayerMask groundLayers;
+	public LayerMask movingPlatformLayers;
+
+	public bool onMovingPlatform = false;
+	private Vector3 lastPlatformPosition;
+	private Vector3 deltaPlatformPosition;
+
+	private Animator animator;
 
 	public string luaFile = "";
 	private Lua lua;
 
+
+	#region UNITY FUNCTIONS
 	// Use this for initialization
 	void Start () {
+		animator = GetComponentInChildren<Animator>();
 		lua = new Lua();
 		lua.DoString("UnityEngine = luanet.UnityEngine");
 		lua.DoString("System = luanet.System");
@@ -17,23 +32,85 @@ public class LuaCharacterController : MonoBehaviour {
 		lua["character"] = this;
 		lua.DoFile(Application.streamingAssetsPath+"/"+luaFile);
 		lua.DoString("Start()");
+		maxHorizontalVelocity = (float)(double)lua.GetNumber("maxSpeed");
 	}
 	
 	// Update is called once per frame
-	void Update(){
-		if (Input.GetKeyDown(KeyCode.LeftArrow)){
+	void Update() {
+
+		lua.DoString(string.Format("Update({0})", Time.deltaTime));
+
+		if (Input.GetKey(KeyCode.LeftArrow)){
 			lua.DoString(string.Format("KeyDown(\"{0}\")", "LeftArrow"));
 		}
-		if (Input.GetKeyDown(KeyCode.RightArrow)){
+		if (Input.GetKey(KeyCode.RightArrow)){
 			lua.DoString(string.Format("KeyDown(\"{0}\")", "RightArrow"));
 		}
 		if (Input.GetKeyDown(KeyCode.UpArrow)){
 			lua.DoString(string.Format("KeyDown(\"{0}\")", "UpArrow"));
 		}
+
+		if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)) {
+			lua.DoString(string.Format("KeyUp(\"{0}\")", "RightArrow"));
+		}
 	}
 
-	public void AddForce(float x, float y) {
-		gameObject.rigidbody2D.AddForce(new Vector2(x,y), ForceMode2D.Impulse);
+	void FixedUpdate() {
+
+		// Check for ground:
+		grounded = Physics2D.OverlapCircle(transform.position, 0.12f, groundLayers);
+
+		// Apply movement:
+		if (movement != Vector2.zero) {
+			if ( ((movement.x < 0) && (gameObject.rigidbody2D.velocity.x <= -maxHorizontalVelocity)) || ((movement.x > 0) && (gameObject.rigidbody2D.velocity.x >= maxHorizontalVelocity)) ) {
+				movement.x = 0;
+			}
+			gameObject.rigidbody2D.AddForce(new Vector2(movement.x, movement.y), ForceMode2D.Impulse);
+		}
+		movement = Vector2.zero;
+
+		// Travel with moving platforms:
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.12f, movingPlatformLayers);
+		if (colliders.Length > 0) {
+			if (onMovingPlatform) {
+				deltaPlatformPosition = colliders[0].transform.position - lastPlatformPosition;
+				float friction = colliders[0].sharedMaterial.friction;
+				gameObject.transform.position += new Vector3(deltaPlatformPosition.x * friction, 0f, 0f);
+			}
+			lastPlatformPosition = colliders[0].transform.position;
+			onMovingPlatform = true;
+
+		}
+		else {
+			onMovingPlatform = false;
+		}
 	}
+
+	#endregion
+
+	#region LUA INTERFACE
+	private void AddForce(float x, float y) {
+		movement = new Vector2(x,y);
+	}
+
+	private void SetAnimation(string state) {
+		switch (state) {
+		case "Idle":
+			animator.SetTrigger("Idle");
+			break;
+		case "Run":
+			animator.SetTrigger("Run");
+			break;
+		case "Jump":
+			animator.SetTrigger("Jump");
+			break;
+		}
+	}
+
+	#endregion
+
+
+
+
 
 }
--- Unity/Assets/StreamingAssets/CharacterControl.lua
+++ Unity/Assets/StreamingAssets/CharacterControl.lua
@@ -1,6 +1,10 @@
- 
-moveSpeed = 0.8
+
+maxSpeed = 1.0 
+moveSpeed = 0.2
+airMoveSpeed = 0.05
 jumpStrength = 3.0
+
+running = false
   
  function Start()
 	-- do this on start
@@ -8,20 +12,43 @@ jumpStrength = 3.0
  
 function Update(dt)
 	-- update loop
+	if not character.grounded then
+		running = false
+		character:SetAnimation("Jump")
+	elseif running then
+		character:SetAnimation("Run")
+	else
+		character:SetAnimation("Idle")
+	end
+	
 end
 
 function KeyDown(key)
 	
-	if key == "LeftArrow" then
-		-- move left
-		character:AddForce(-moveSpeed, 0)
-	elseif key == "RightArrow" then
-		-- move right
-		character:AddForce(moveSpeed, 0)
+	if character.grounded then
+		-- move on ground:
+		if key == "LeftArrow" then
+			character:AddForce(-moveSpeed, 0)
+			running = true
+		elseif key == "RightArrow" then
+			character:AddForce(moveSpeed, 0)
+			running = true
+		end
+		-- jump:
+		if key == "UpArrow" then
+			character:AddForce(0, jumpStrength)
+		end
+	else
+		-- move in air:
+		if key == "LeftArrow" then
+			character:AddForce(-airMoveSpeed, 0)
+		elseif key == "RightArrow" then
+			character:AddForce(airMoveSpeed, 0)
+		end
 	end
-	
-	if key == "UpArrow" then
-		-- jump
-		character:AddForce(0, jumpStrength)
-	end
+
+end
+
+function KeyUp(key)
+	running = false
 end
