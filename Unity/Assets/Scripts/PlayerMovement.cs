6c414a72aa169391f4ebda624e136e8cd54c7bad
Rich Metson <richardmetson@gmail.com>
2015-02-17 22:55:34 +0000

    Set up jumping blend tree states and animation

--- Unity/Assets/Scripts/PlayerMovement.cs
+++ Unity/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,15 @@ public class PlayerMovement : MonoBehaviour {
 
 	Animator anim;
 
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+
+	public float jumpForce = 100f;
+
+	bool doubleJumpUsed = false;
+
 	public Vector3 velocity = Vector3.zero;
 	public Vector3 gravity;
 	public Vector3 moveLeftVel;
@@ -29,7 +38,14 @@ public class PlayerMovement : MonoBehaviour {
 	// Do Input and Graphics updates here
 	void Update()
 	{
+		if((grounded || !doubleJumpUsed) && Input.GetKeyDown(KeyCode.Space))
+		{
+			anim.SetBool("Grounded", false);
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));
 
+			if (!doubleJumpUsed && !grounded)
+			{ doubleJumpUsed = true; }
+		}
 		/*
 		 * Old movement code
 		 *
@@ -52,11 +68,21 @@ public class PlayerMovement : MonoBehaviour {
 	// Do physics engine updates here
 	void FixedUpdate () 
 	{
+		// if result is true we are on ground, if not we are not on ground
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+		// set the animator parameter 'Grounded' to match bool grounded above)
+		anim.SetBool("Grounded", grounded);
 
+		if(grounded){doubleJumpUsed = false;}
+
+		anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+
+		// Check horizontal axis input and save to float 'move'
 		float move = Input.GetAxis ("Horizontal");
-
+		// set the animator parameter 'Speed' to match float 'move' above)
 		anim.SetFloat("Speed", Mathf.Abs(move));
 
+		// make the rigid body's velocity match move and cap it at MaxSpeed, and set the y axis to stay at the current position
 		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
 
 		if (move > 0 && !facingRight)
