65f3c5ab08bd94342896dd03d3a5f3c7e8702db4
Rich Metson <richardmetson@gmail.com>
2015-02-17 21:16:05 +0000

    setup character controller to use 2D physics and move rigid body 2D velocity.
    Added Walk state into animator

--- Unity/Assets/Scripts/PlayerMovement.cs
+++ Unity/Assets/Scripts/PlayerMovement.cs
@@ -3,12 +3,17 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+	public float maxSpeed;
+	bool facingRight = true;
+
+	Animator anim;
+
 	public Vector3 velocity = Vector3.zero;
 	public Vector3 gravity;
 	public Vector3 moveLeftVel;
 	public Vector3 moveRightVel;
 	public Vector3 jumpHeight;
-	public float maxSpeed;
+
 
 	bool moveLeft = false;
 	bool moveRight = false;
@@ -17,24 +22,55 @@ public class PlayerMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	
+		anim = GetComponent<Animator>();
+
 	}
 
 	// Do Input and Graphics updates here
-	void Update(){
-		if (Input.GetKeyDown(KeyCode.LeftArrow)){
-			moveLeft = true;
-		}
-		if (Input.GetKeyDown(KeyCode.RightArrow)){
-			moveRight = true;
-		}
-		if (Input.GetKeyDown(KeyCode.UpArrow)){
-			jump = true;
-		}
+	void Update()
+	{
+
+		/*
+		 * Old movement code
+		 *
+
+				if (Input.GetKeyDown(KeyCode.LeftArrow)){
+					moveLeft = true;
+				}
+				if (Input.GetKeyDown(KeyCode.RightArrow)){
+					moveRight = true;
+				}
+				if (Input.GetKeyDown(KeyCode.UpArrow)){
+					jump = true;
+				}
+
+		*/
+
 	}
 
 	
 	// Do physics engine updates here
-	void FixedUpdate () {
+	void FixedUpdate () 
+	{
+
+		float move = Input.GetAxis ("Horizontal");
+
+		anim.SetFloat("Speed", Mathf.Abs(move));
+
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+		if (move > 0 && !facingRight)
+		{
+			Flip();
+		}
+		else if (move < 0 && facingRight)
+		{
+			Flip();
+		}
+
+/*
+ * Old movement code
+ * 
 
 		velocity += gravity * Time.deltaTime;
 
@@ -58,8 +94,19 @@ public class PlayerMovement : MonoBehaviour {
 
 		transform.position += velocity * Time.deltaTime;
 
+*/
+
 
 
 	
+	}
+
+	void Flip (){
+
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+
 	}
 }
