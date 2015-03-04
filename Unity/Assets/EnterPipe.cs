49ca47bf8957f818bba69b20dfbb89175ec01118
Rich Metson <richardmetson@gmail.com>
2015-03-04 12:12:35 +0000

    fixed pipe teleportation. removed some old sprite sheets

--- Unity/Assets/EnterPipe.cs
+++ Unity/Assets/EnterPipe.cs
@@ -5,15 +5,16 @@ public class EnterPipe : MonoBehaviour {
 
     public int nextLevel = 1;
 
-    void OnTriggerEnter(Collider col)
+    void OnTriggerEnter2D (Collider2D col)
     {
         //TODO stop character controller
         //TODO animate player going down pipe
         //TODO play pipe sound
 
         // wait .. seconds and load level
-        Invoke("LoadNextLevel", 1);
         Debug.Log("loading next level");
+        Invoke("LoadNextLevel", 1);
+        
        
 
     }
