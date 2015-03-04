7c950c8d3e6521f567b2385a15a96be1c37d367c
Rich Metson <richardmetson@gmail.com>
2015-03-04 19:40:13 +0000

    Merging working anims and speech bubble from master local to origin/master remote

---
+++ Unity/Assets/OnHitSpikes.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnHitSpikes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        other.gameObject.SendMessage("PlayerFail");
+    }
+}
