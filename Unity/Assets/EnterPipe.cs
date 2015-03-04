94623865d5969d06569eeb75d1d437396bca679a
Rich Metson <richardmetson@gmail.com>
2015-03-04 00:18:55 +0000

    Added blank prefabs for Level2-BlankWorld and setup basic level load script on pipe, although currently not working (collider detection issues possibly)

---
+++ Unity/Assets/EnterPipe.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterPipe : MonoBehaviour {
+
+    public int nextLevel = 1;
+
+    void OnTriggerEnter(Collider col)
+    {
+        //TODO stop character controller
+        //TODO animate player going down pipe
+        //TODO play pipe sound
+
+        // wait .. seconds and load level
+        Invoke("LoadNextLevel", 1);
+        Debug.Log("loading next level");
+       
+
+    }
+
+    void LoadNextLevel() { Application.LoadLevel(nextLevel); }
+}
