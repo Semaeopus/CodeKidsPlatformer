ab2c07897ec1b4a871515fcb70be813043f356e7
Rich Metson <richardmetson@gmail.com>
2015-03-04 23:08:10 +0000

    reorganised spritesheet folder, allowed for replaceable spritesheets, move source art into own folder

---
+++ Unity/Assets/StreamingAssets/Level2Scripts/CharacterControl-lvl-02.lua
@@ -0,0 +1,69 @@
+
+weight = 2.5
+maxSpeed = 5.0
+moveSpeed = 1.0
+airMoveSpeed = 0.2
+jumpStrength = 12.0
+
+running = false
+movement = {x = 0, y = 0}
+
+ function Start()
+	-- do this on start
+ end
+
+function Update(dt)
+	-- update loop
+
+	-- set right animation:
+	if not character.grounded then
+		running = false
+		character:SetAnimation("Jump")
+	elseif running then
+		character:SetAnimation("Run")
+	else
+		character:SetAnimation("Idle")
+	end
+
+	-- apply movement and then reset it:
+	character:AddForce(movement.x, movement.y)
+	movement = {x = 0, y = 0}
+
+end
+
+function ButtonDown(button)
+	if character.grounded then
+		-- jump:
+		if button == "Jump" then
+			movement.y= movement.y + jumpStrength
+		end
+		-- move on ground:
+		if button == "MoveLeft" then
+			movement.x = movement.x - moveSpeed
+			running = true
+		elseif button == "MoveRight" then
+			movement.x = movement.x + moveSpeed
+			running = true
+		end
+	else
+		-- move in air:
+		if button == "MoveLeft" then
+			--movement.x = movement.x - airMoveSpeed
+		elseif button == "MoveRight" then
+			movement.x = movement.x + airMoveSpeed
+		end
+	end
+end
+
+function ButtonUp(button)
+	running = false
+end
+
+function PlayerFail()
+	-- play fail animation
+	character:SetAnimation("Fail")
+
+	-- apply bounce force up from spikes
+	character:AddForce(0, 20) -- or possibly (movement.x -4, -movement.y)
+
+	end
---
+++ Unity/Assets/StreamingAssets/Level2Scripts/WorldController-lvl-02.lua
@@ -0,0 +1,13 @@
+
+-- this should probably be 9.81
+gravity = 9.81
+
+
+function Start()
+	-- do this on start
+   -- world:SetGravity(gravity)
+end
+
+function Update(dt)
+	-- update loop
+end
